6919370bb6961a0090c59924b11a55e01ca6677b
Godfred-A99 <godfredadjetey99@gmail.com>
2023-09-30 01:51:50 -0700

    added new functions

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
-const btnPopup = document.querySelector('.btnlogin-popup');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
 
 registerLink.addEventListener('click', ()=> {
     wrapper.classList.add('active')
@@ -13,6 +14,10 @@ loginLink.addEventListener('click', ()=> {
 
 btnPopup.addEventListener('click', ()=> {
     wrapper.classList.add('active-popup')
+});
+
+iconClose.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup')
 });
 
 
